7a846193f37fedb198c360dcfc88fe06d46c1ccf
Ratanak Pek <ratanakpek088@gmail.com>
2017-10-10 23:09:51 +0700

    test library

--- app/src/main/java/rpek/activityvsfragmentdemo/views/MainActivity.kt
+++ app/src/main/java/rpek/activityvsfragmentdemo/views/MainActivity.kt
@@ -2,16 +2,24 @@ package rpek.activityvsfragmentdemo.views
 
 
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.muddzdev.styleabletoastlibrary.StyleableToast
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_main.*
 import rpek.activityvsfragmentdemo.R
 import rpek.activityvsfragmentdemo.fragments.CartFragment
 import rpek.activityvsfragmentdemo.fragments.HomeFragment
 import rpek.activityvsfragmentdemo.fragments.SignInFragment
+import android.view.Gravity
+import android.view.View
+import com.github.florent37.awesomebar.ActionItem
+import com.github.florent37.awesomebar.AwesomeBar
+
+
 
 class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener, CartFragment.OnFragmentInteractionListener, SignInFragment.OnFragmentInteractionListener {
 
@@ -23,26 +31,44 @@ class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionList
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+       // var drawerLayout = drawerLayout
+
+        bar.addAction(R.drawable.awsb_ic_edit_animated, "Compose");
+        bar.setActionItemClickListener { position, actionItem -> Toast.makeText(baseContext, actionItem.text + " clicked", Toast.LENGTH_LONG).show() }
+
+//        bar.setOnMenuClickedListener(object : View.OnClickListener() {
+//            fun onClick(v: View) {
+//                 drawerLayout.openDrawer(Gravity.START)
+//            }
+//        })
+
+        //bar.displayHomeAsUpEnabled(true / false)
+
+
         supportFragmentManager.inTransaction {
             add(R.id.fragment_container, HomeFragment())
         }
 
-        bottomNavigationView.setOnNavigationItemSelectedListener{
-            item->
-            when(item.itemId){
-                R.id.action_home->{
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            item ->
+            when (item.itemId) {
+                R.id.action_home -> {
                     supportFragmentManager.inTransaction {
                         replace(R.id.fragment_container, HomeFragment())
+                        //Toasty.success(this@MainActivity, "HomeScreen", Toast.LENGTH_SHORT).show()
+                        StyleableToast.makeText(this@MainActivity, "Hello World!", Toast.LENGTH_LONG, R.style.MyToast).show();
                     }
                 }
-                R.id.action_offline->{
+                R.id.action_offline -> {
                     supportFragmentManager.inTransaction {
                         replace(R.id.fragment_container, CartFragment())
+                        //Toasty.error(this@MainActivity, "Offline", Toast.LENGTH_SHORT).show()
                     }
                 }
-                R.id.action_user->{
+                R.id.action_user -> {
                     supportFragmentManager.inTransaction {
                         replace(R.id.fragment_container, SignInFragment())
+                       // Toasty.normal(this@MainActivity, "SignIn", Toast.LENGTH_SHORT).show()
                     }
                 }
 
@@ -51,7 +77,6 @@ class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionList
         }
 
     }
-
 
 
     inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
